1a091115882252fdfbe6e4955b81ba559d2d9fc9
ssandyy <ssandyy789@gmail.com>
2025-06-19 11:28:51 +0530

    JSBrushUp, datatype, Comparation, Operation ..

---
+++ JSBrushUp/basics/comparasion.js
@@ -0,0 +1,9 @@
+
+console.log(2==2);
+console.log(2>1);
+console.log(1<2);
+console.log(3);
+console.log();
+console.log();
+console.log();
+console.log();
---
+++ JSBrushUp/basics/operations.js
@@ -0,0 +1,10 @@
+console.log(2+2);//4
+console.log("2"+4);//24
+console.log(2+"5");//25
+
+console.log(2 + 4 + "3"); //63
+
+console.log("3" + 4 +2); //342
+
+// Note JS perform operation as per first data type..!
+
