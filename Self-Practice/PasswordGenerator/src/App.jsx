4cb688ee71af55f190417087757d5a223f30b247
ssandyy <ssandyy789@gmail.com>
2025-06-20 16:07:39 +0530

    password generator logic completed

--- Self-Practice/PasswordGenerator/src/App.jsx
+++ Self-Practice/PasswordGenerator/src/App.jsx
@@ -1,31 +1,34 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const[length, setLength] = useState(8)
   const[numCheck, setNumCheck] = useState(false)
-  const[specialCharCheck, setspecialCharCheck] = useState(true)
+  const[specialCharCheck, setspecialCharCheck] = useState(false)
   const[password, setPassword] = useState("")
 
   // const passwordGenerator = useCallback(() => {}, [length, specialCharCheck, numCheck, setPassword]) 
+ 
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-
     if(numCheck){  // if numbers is allowed then concat with string
       return str+= "0123456789"
     }
-    if (specialCharCheck) str += "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    if (specialCharCheck) str += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~" ; // same line doesnot need return decleration
 
     for (let i = 0; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass= str.charAt(char)
+      pass += str.charAt(char)
     }
     setPassword(pass)
-
   }, [length, specialCharCheck, numCheck, setPassword]) 
 
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, specialCharCheck, numCheck, passwordGenerator])
 
   return (
     <>
@@ -39,9 +42,39 @@ function App() {
         className="outline-none w-full p-1 px-3"
         placeholder='Generated Password..'
         />
+        <button>Copy</button>
+      </div>
+      
+      <div className='flex text-sm gap-3 items-center'>
+        <input 
+          type="range" 
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(events) => setLength(events.target.value)}
+        />
+        <label htmlFor="">{length}</label>
 
+        <input 
+          type="checkbox"
+          defaultChecked={numCheck}
+          className='cursor-pointer'
+          onClick={() => setNumCheck((prev) => !prev)}   
+        />
+         <label htmlFor="numberInput">Number </label>
 
-        <button onClick={passwordGenerator}></button>
+        <input 
+          type="checkbox" 
+          defaultChecked={specialCharCheck} 
+          className='cursor-pointer' 
+          onClick={() => setspecialCharCheck((prev) => !prev)}
+        />
+        <label htmlFor="special character generator" >Special Character</label>
+
+      </div>
+      <div className="flex flex-wrap text-sm gap-3 items-center">
+
       </div>
     </div>
     </>
