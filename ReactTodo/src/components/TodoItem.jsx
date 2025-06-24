3372161f3e898f9f0ad77426bb3557a6468a8573
ssandyy <ssandyy789@gmail.com>
2025-06-24 10:55:19 +0530

    ReactTodo finalised..

--- ReactTodo/src/components/TodoItem.jsx
+++ ReactTodo/src/components/TodoItem.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
 
-const TodoItem = () => {
+const TodoItem = ({todo}) => {
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+
+    const {updateTodo, toggleComplete, deleteTodo} = useTodo()
+
+    const editTodo = () => {
+    updateTodo(todo.id, {...todo, todo: todoMsg})
+    setIsTodoEditable(false)
+  }
+  const toggleCompleted = () => {
+    //console.log(todo.id);
+    toggleComplete(todo.id)
+  }
+
+
   return (
     <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
